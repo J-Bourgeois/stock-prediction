f4a394c05524c045f2c02b6c70ac6017e4b1b1e4
Jacob Bourgeois <j.ma.bourgeois@outlook.com>
2025-04-28 12:00:48 -0400

    Added sidebar from shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-w-10/12 min-h-10/12 flex-col items-center justify-center">
+      <h1>Stock Prediction</h1>
       
     </div>
   );
