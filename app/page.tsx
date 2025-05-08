9b18858a09d35410f90fd3828166864a3c2849dd
Jacob Bourgeois <j.ma.bourgeois@outlook.com>
2025-05-08 12:25:56 -0400

    Added margin on the right side for bigger screens to offset the sidebar to correctly center the elements

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import StockPricesResponse from "@/components/StockPricesResponse";
 
 export default function Home() {
   return (
-    <div className="flex m-auto min-h-dvh min-w-full flex-col justify-center">
-      <h1 className="mt-4 text-center">Stock Prediction</h1>
-      <div className="flex flex-col max-w-sm min-w-sm m-auto">
+    <div className="flex xs:mr-7 min-h-dvh max-w-full w-full flex-col justify-center">
+      <h1 className="mt-4 text-center xs:m-auto">Stock Prediction</h1>
+      <div className="flex flex-col max-w-sm min-w-sm m-auto xs:mr-auto">
         <StockPricesResponse />
       </div>
     </div>
