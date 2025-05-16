b4ba31e3e75502db47e2a7952dee2b735f349ae0
Jacob Bourgeois <j.ma.bourgeois@outlook.com>
2025-05-16 09:49:52 -0400

    Changed h1 to be absolute positioned to avoid sidebar opening to cause it to collide with other elements

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import TimeSpanSelector from "@/components/TimeSpanSelector";
 export default function Home() {
   return (
     <div className="flex xs:mr-7 min-h-dvh max-w-full w-full flex-col justify-center">
-      <h1 className="max-xs:pt-15 max-sm:pt-15 pt-3 pb-6 text-center xs:m-auto">Stock Prediction</h1>
+      <h1 className="max-sm:pt-15 sm:absolute -translate-x-1/2 left-6/12 top-3 pb-6 text-center xs:m-auto">
+        Stock Prediction
+      </h1>
       <div className="absolute top-2 right-18">
         <TimeSpanSelector />
       </div>
-      <div className="relative flex flex-col xs:w-9/12 max-xs:min-w-sm m-auto xs:mr-auto">
+      <div className="relative flex flex-col pt-6 xs:w-9/12 max-xs:min-w-sm m-auto xs:mr-auto">
         <HomeStockPricesResponse />
       </div>
     </div>
