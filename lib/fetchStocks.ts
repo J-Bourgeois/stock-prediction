7b440f794661ffa1946d6450495747b5b18b5988
Jacob Bourgeois <j.ma.bourgeois@outlook.com>
2025-05-21 10:19:23 -0400

    Added revalidate time of 1 hour, So API call gets called once every hour to lighten the load on the server & client

--- lib/fetchStocks.ts
+++ lib/fetchStocks.ts
@@ -2,18 +2,26 @@ import { formatDate } from "@/app/types/dateFunctions";
 
 const apiKey = process.env.STOCKDATA_API_KEY;
 
-
 export async function fetchNvidiaStock() {
-  const res = await fetch(`https://api.stockdata.org/v1/data/intraday?symbols=NVDA&interval=hour&sort=asc&data_to=${formatDate}&api_token=${apiKey}`);
+  const res = await fetch(
+    `https://api.stockdata.org/v1/data/intraday?symbols=NVDA&interval=hour&sort=asc&data_to=${formatDate}&api_token=${apiKey}`,
+    { next: { revalidate: 3600 } }
+  );
   return await res.json();
 }
 
 export async function fetchAppleStock() {
-  const res = await fetch(`https://api.stockdata.org/v1/data/intraday?symbols=AAPL&interval=hour&sort=asc&data_to=${formatDate}&api_token=${apiKey}`);
+  const res = await fetch(
+    `https://api.stockdata.org/v1/data/intraday?symbols=AAPL&interval=hour&sort=asc&data_to=${formatDate}&api_token=${apiKey}`,
+    { next: { revalidate: 3600 } }
+  );
   return await res.json();
 }
 
 export async function fetchMicrosoftStock() {
-  const res = await fetch(`https://api.stockdata.org/v1/data/intraday?symbols=MSFT&interval=hour&sort=asc&data_to=${formatDate}&api_token=${apiKey}`);
+  const res = await fetch(
+    `https://api.stockdata.org/v1/data/intraday?symbols=MSFT&interval=hour&sort=asc&data_to=${formatDate}&api_token=${apiKey}`,
+    { next: { revalidate: 3600 } }
+  );
   return await res.json();
 }
