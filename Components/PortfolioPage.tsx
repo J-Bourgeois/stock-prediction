53474a07a630c3f8dd0d471ecb76d2a3148ddfa9
Jacob Bourgeois <j.ma.bourgeois@outlook.com>
2025-05-30 13:47:59 -0400

    Further updated portfolio page to dynamically add stock charts based on users portfolio

--- Components/PortfolioPage.tsx
+++ Components/PortfolioPage.tsx
@@ -1,14 +1,19 @@
 import prisma from "@/lib/prisma";
 import { verifyJwt } from "@/lib/session";
 import { cookies } from "next/headers";
+import PortfolioStockPricesResponse from "./PortfolioStockPricesResponse";
 
 interface Props {
-  nvidiaStock: any,
-  appleStock: any,
-  microsoftStock: any
+  nvidiaStock: any;
+  appleStock: any;
+  microsoftStock: any;
 }
 
-export async function PortfolioPage({nvidiaStock, appleStock, microsoftStock}: Props) {
+export async function PortfolioPage({
+  nvidiaStock,
+  appleStock,
+  microsoftStock,
+}: Props) {
   const cookieStore = await cookies();
   const cookie = cookieStore.get("token")?.value;
   const session = await verifyJwt(cookie);
@@ -22,20 +27,28 @@ export async function PortfolioPage({nvidiaStock, appleStock, microsoftStock}: P
     include: {
       Portfolio: {
         include: {
-          stockSelections: true,
+          stockSelections: {
+            include: {
+              stock: true,
+            },
+          },
         },
       },
     },
   });
 
-
   return (
-    <div className="flex flex-col w-full h-screen">
+    <div className="flex flex-col min-w-full w-full text-center items-center">
       {user?.Portfolio && user.Portfolio[0]?.stockSelections.length > 0 ? (
-        user.Portfolio[0].stockSelections.map((item, index) => (
-        <div key={index}>
-
-        </div>))
+        user.Portfolio[0].stockSelections.map((stockSelection) => (
+          <PortfolioStockPricesResponse
+            key={stockSelection.stock.id}
+            stockData={stockSelection.stock}
+            nvidiaStock={nvidiaStock}
+            appleStock={appleStock}
+            microsoftStock={microsoftStock}
+          />
+        ))
       ) : (
         <p className="p-6 m-auto text-slate-400">
           Oops! Looks like you don't have any stocks in your portfolio! Go to
---
+++ Components/PortfolioStockPricesResponse.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/store/Store";
+
+import { hydrateHomeStocksNvidia } from "@/store/homeStocksNvidiaSlice";
+import { hydrateHomeStocksApple } from "@/store/homeStocksAppleSlice";
+import { hydrateHomeStocksMicrosoft } from "@/store/homeStocksMicrosoftSliceSlice";
+
+import { StocksChart } from "./StocksChart";
+import { Button } from "./ui/button";
+import { homeStocksApi } from "@/app/types/homeStocksInterface";
+import { ActionCreatorWithPayload, Dispatch } from "@reduxjs/toolkit";
+
+
+interface PortfolioStockProps {
+  stockData: {
+    symbol: string;
+    id: string;
+    name: string;
+  };
+  nvidiaStock: homeStocksApi;
+  appleStock: homeStocksApi;
+  microsoftStock: homeStocksApi;
+}
+
+
+
+export default async function PortfolioStockPricesResponse({
+  stockData,
+  nvidiaStock,
+  appleStock,
+  microsoftStock
+}: PortfolioStockProps) {
+    const dispatch = useDispatch<AppDispatch>();
+
+    const nvidiaData = useSelector((state: RootState) => state.homeStocksNvidia);
+    const appleData = useSelector((state: RootState) => state.homeStocksApple);
+    const microsoftData = useSelector((state: RootState) => state.homeStocksMicrosoft);
+
+    const hydrateIfEmpty = (dataArray: homeStocksApi, action: ActionCreatorWithPayload<homeStocksApi>) => {
+        if (dataArray.data.length === 0) action(stockData);
+    }
+
+    useEffect(() => {
+        hydrateIfEmpty(nvidiaData, hydrateHomeStocksNvidia(nvidiaStock));
+        nvidiaData.data.length === 0 && dispatch(hydrateHomeStocksNvidia(nvidiaStock));
+        appleData.data.length === 0 && dispatch(hydrateHomeStocksApple(appleStock));
+        microsoftData.data.length === 0 && dispatch(hydrateHomeStocksMicrosoft(microsoftStock));
+    }, [dispatch, nvidiaStock, appleStock, microsoftStock]);
+
+  return (
+    <div className="relative pb-6 min-w-[300px] w-[80vw] max-w-[1200px] flex flex-col text-sm">
+      <h2 className="absolute top-2 left-2 max-w-6/12">{stockData.name}</h2>
+      <p className="absolute top-2 right-2">
+        {stockData.symbol}
+      </p>
+      <div className="border border-foreground rounded-xl mt-8">
+        <StocksChart chartData={homeStocksNvidia} chartConfig={chartConfig} />
+      </div>
+      <div className="pt-4">
+        <Button
+          onClick={() => addPortfolioStock(homeStocksNvidia.data[0].ticker)}
+        >
+          Ask A.I if you should buy this stock!
+        </Button>
+      </div>
+    </div>
+  );
+}
