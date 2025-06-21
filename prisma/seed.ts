3223d3c148ab71f2e788ed1d060b879dfcf16f8d
Jacob Bourgeois <j.ma.bourgeois@outlook.com>
2025-06-21 15:10:21 -0400

    Added script for Git cloning to run all necessary commands

---
+++ prisma/seed.ts
@@ -0,0 +1,31 @@
+// prisma/seed.ts
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const stocks = [
+    { symbol: 'NVDA', name: 'Nvidia Corporation' },
+    { symbol: 'AAPL', name: 'Apple Inc.' },
+    { symbol: 'MSFT', name: 'Microsoft Corp' },
+  ];
+
+  for (const stock of stocks) {
+    await prisma.stock.upsert({
+      where: { symbol: stock.symbol },
+      update: {},
+      create: stock,
+    });
+  }
+
+  console.log('✅ Seed completed.');
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(() => {
+    prisma.$disconnect();
+  });
